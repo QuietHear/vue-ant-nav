9283395a4ead5b5137e95c872746629b4ba36bbd
起床啦！ <1106728884@qq.com>
2019-04-30 13:37:52 +0800

    目录搭建

---
+++ lib/vueAntNav.vue
@@ -0,0 +1,23 @@
+/*
+* @Author: aFei
+* @Date: 2019-04-30 11:26:44
+*/
+/*
+* @LastEditors: aFei
+* @LastEditTime: 2019-04-30 11:26:46
+*/
+<template>
+    <div>
+        1
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "vueAntNav"
+    }
+</script>
+
+<style scoped>
+
+</style>
